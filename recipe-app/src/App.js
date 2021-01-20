f92c2b1e4f036cae5015972e9652ef607417f410
Tanya Dixit <shanya.2018td@gmail.com>
2021-01-21 00:22:02 +0530

    sent request

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React,{useEffect} from "react";
 import './App.css';
 
 const App = () => {
  
-  const APP_ID = 'f6f7f8f9';
-  const APP_KEY = 'hfdjsfiajdj8837583bjh78yyaw';
+  const APP_ID = '56f565f7';
+  const APP_KEY = '280dbbe1db900ba64f3ed77158c7ab55';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+    const data = await response.json();
+    console.log(data);
+  }
 
   return(
     <div className = "App">
