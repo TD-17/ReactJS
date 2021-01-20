1767b6a706f2c97daeb982925cb7fddde67ce732
Tanya Dixit <shanya.2018td@gmail.com>
2021-01-20 20:37:08 +0530

    minor changes

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const APP_ID = 'f6f7f8f9';
   const APP_KEY = 'hfdjsfiajdj8837583bjh78yyaw';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
   return(
     <div className = "App">
