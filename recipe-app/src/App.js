1bd268cf36109a73a2cd051f0bc8c7edb98afb04
Tanya Dixit <shanya.2018td@gmail.com>
2021-01-20 20:19:16 +0530

    changed App.js file

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-const app = () => [
+const App = () => {
+ 
+  const APP_ID = 'f6f7f8f9';
+  const APP_KEY = 'hfdjsfiajdj8837583bjh78yyaw';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
   return(
-    
+    <div className = "App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+    </div>
   );
-]
+  }
 
 export default App;
